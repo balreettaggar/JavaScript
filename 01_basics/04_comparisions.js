edab33948e1e9e169c4282f71c9bde9377ed67a4
balreettaggar <133488554+balreettaggar@users.noreply.github.com>
2024-12-02 07:06:39 +0000

    comparisions

---
+++ 01_basics/04_comparisions.js
@@ -0,0 +1,26 @@
+// console.log(2>1);
+// console.log(2<=1);
+// these comparisions will return a boolean value (true or false)
+
+// console.log("2">1);
+// console.log("02">1);
+// js converted strings into numbers for conversion 
+// but sometimees the comparision between different datatypes gives unusual result
+
+// console.log(null > 0); // null has empty value
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// equality operator == and comparision operator >,<,>=,<= work differently
+// equality operator just checks the value whereas the comparision operator changes the datatype
+// null is converted into 0 anc checked whether is right or not so line no 12 gives true value
+
+// console.log(undefined > 0);
+// console.log(undefined == 0)
+// console.log(undefined < 0);
+// it will return false cuz undefined has no value
+
+// === checks values and datatypes as well
+// == converts the datatype and checks the value
+// console.log("3"==3); // true
+// console.log("3"===3); // false
